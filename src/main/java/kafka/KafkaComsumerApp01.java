eb5ae5eba1aca2f8503cd0fc573e6c2092111a11
spyAcc <2548876189@qq.com>
2018-11-15 17:00:55 +0800

    kafka

---
+++ src/main/java/kafka/KafkaComsumerApp01.java
@@ -0,0 +1,41 @@
+package kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * kafka 消费者，消费消息
+ */
+public class KafkaComsumerApp01 {
+
+    public static void main(String[] args) {
+
+        Properties prop = new Properties();
+        prop.put("bootstrap.servers", "localhost:9092");
+        prop.put("zookeeper", "localhost:2181");
+        prop.put("group.id", "test-1");
+        prop.put("enable.auto.commit", "true");
+        prop.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        prop.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(prop);
+        consumer.subscribe(Arrays.asList("test"));
+
+        while(true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for(ConsumerRecord<String, String> record: records) {
+                System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
+                System.out.println();
+            }
+        }
+
+
+    }
+
+
+
+}
---
+++ src/main/java/kafka/KafkaProducerApp01.java
@@ -0,0 +1,35 @@
+package kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * kafka 生产者，生产消息
+ */
+public class KafkaProducerApp01 {
+
+    public static void main(String[] args) {
+
+        Properties prop = new Properties();
+        prop.put("bootstrap.servers", "localhost:9092");
+        prop.put("batch.size", 5);
+        prop.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        prop.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(prop);
+        for(int i = 101; i < 200; i++) {
+            producer.send(new ProducerRecord<>("test", Integer.toString(i), Integer.toString(i) ));
+            System.out.println(i);
+        }
+
+        producer.close();
+
+    }
+
+
+
+
+}
